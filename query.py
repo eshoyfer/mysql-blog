3e560e58fd83008f5a3c6dd4dc7f6bdc3eceec63
ChristopherWennendy <christopherwennendy@gmail.com>
2014-10-22 11:45:34 -0400

    Update query.py

--- query.py
+++ query.py
@@ -1,1 +1,11 @@
-asdf
+import sqlite3
+conn = sqlite3.connect("p.db")
+
+c = conn.cursor()
+
+q = "select * from posts, text post"
+
+result = c.execute(q)
+print result
+for r in result:
+    print r
