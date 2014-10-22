77aab28cb6f6863750b23ffed40383376e559963
l-tn3314 <l.tn3314@gmail.com>
2014-10-22 11:43:24 -0400

    added form

--- app.py
+++ app.py
@@ -6,10 +6,13 @@ app = Flask(__name__)
 @app.route("/home")
 @app.route("/")
 def home():
+    #dat = request.args.get(
     return render_template("home.html")
+
 @app.route("/title")
 def title():
     return render_template("title.html")
+
 
 if __name__=="__main__":
     app.debug=True
