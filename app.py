f1ff12e47b0afe54a32a063303612fd6012c5484
l-tn3314 <l.tn3314@gmail.com>
2014-10-22 21:49:30 -0400

    form accepting responses

--- app.py
+++ app.py
@@ -1,13 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__)
 
 
-@app.route("/home")
-@app.route("/")
+def toTable(t,p):
+
+    conn = sqlite3.connect('blog.db')
+    c = conn.cursor()
+
+    c.execute('''create table if not exists posts(title text, post text)''')
+    c.execute("INSERT INTO posts VALUES('" + t + "','" + p + "')")
+    print c.fetchall()
+    conn.commit()
+    conn.close()
+    
+
+@app.route("/home", methods = ["GET","POST"])
+@app.route("/", methods = ["GET","POST"])
 def home():
-    #dat = request.args.get(
-    return render_template("home.html")
+    if request.method == "GET":
+        return render_template("home.html")
+    else:
+        title = request.form["title"]
+        post = request.form["post"]
+        #button = request.form["b"]
+        toTable(title,post)
+        return render_template("home.html")
+        
+        
 
 @app.route("/title")
 def title():
